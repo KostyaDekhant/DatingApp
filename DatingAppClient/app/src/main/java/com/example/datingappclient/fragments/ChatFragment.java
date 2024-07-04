2db9504500406c7c9f1085c5b65d78e226d7ee8b
Enigmater <ivan.tihonov1999@mail.ru>
2024-07-04 20:47:42 +0700

    Intermediate commit

--- DatingAppClient/app/src/main/java/com/example/datingappclient/fragments/ChatFragment.java
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/fragments/ChatFragment.java
@@ -1,20 +1,26 @@
 package com.example.datingappclient.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.datingappclient.Message;
 import com.example.datingappclient.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.StompClient;
 
 
 public class ChatFragment extends Fragment {
@@ -45,9 +51,27 @@ public class ChatFragment extends Fragment {
 
         MaterialButton sendButton = acticityView.findViewById(R.id.sendmess_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("CheckResult")
             @Override
             public void onClick(View view) {
+                StompClient stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, "ws://26.223.19.56:8080/datingapp");
+                stompClient.connect();
+                stompClient.lifecycle().subscribe(lifecycleEvent -> {
+                    switch (lifecycleEvent.getType()) {
+                        case OPENED:
+                            Log.d("TAG", "Stomp connection opened");
+                            break;
 
+                        case ERROR:
+                            Log.e("TAG", "Error", lifecycleEvent.getException());
+                            break;
+
+                        case CLOSED:
+                            Log.d("TAG", "Stomp connection closed");
+                            break;
+                    }
+                });
+                stompClient.send("/api/send", new Message(editText.getText().toString(), getCurrentTimeStamp(), 24, reciverID).toString());
             }
         });
 
