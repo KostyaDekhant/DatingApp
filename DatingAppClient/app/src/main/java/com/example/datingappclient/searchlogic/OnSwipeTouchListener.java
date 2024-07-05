e17c2c9a3f77c932a297e877ef7e65bf4877a4af
Alexey <98819212+roroai@users.noreply.github.com>
2024-07-05 16:29:57 +0700

    some search changes

---
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/searchlogic/OnSwipeTouchListener.java
@@ -0,0 +1,68 @@
+package com.example.datingappclient.searchlogic;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class OnSwipeTouchListener implements View.OnTouchListener {
+
+    private static final String TAG = "OnSwipeTouchListener";
+    private final GestureDetector gestureDetector;
+
+    public OnSwipeTouchListener(Context context) {
+        gestureDetector = new GestureDetector(context, new GestureListener());
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        Log.d(TAG, "onTouch event: " + event.toString());
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.d(TAG, "onDown: " + e.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(TAG, "onFling: e1=" + e1.toString() + " e2=" + e2.toString() + " velocityX=" + velocityX + " velocityY=" + velocityY);
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            Log.d(TAG, "onSwipeRight");
+                            onSwipeRight();
+                        } else {
+                            Log.d(TAG, "onSwipeLeft");
+                            onSwipeLeft();
+                        }
+                        result = true;
+                    }
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+    }
+
+    public void onSwipeRight() {
+        Log.d(TAG, "Swiped Right");
+    }
+
+    public void onSwipeLeft() {
+        Log.d(TAG, "Swiped Left");
+    }
+}
