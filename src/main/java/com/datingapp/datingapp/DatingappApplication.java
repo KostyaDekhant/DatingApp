75b362dc14b84d5a0b8c78dc997a74634f45ab7b
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2024-07-10 16:04:40 +0700

    add database

--- src/main/java/com/datingapp/datingapp/DatingappApplication.java
+++ src/main/java/com/datingapp/datingapp/DatingappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class DatingappApplication {
+public class DatingappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatingappApplication.class, args);
