09ef8010c8815b3b4e09d032b757009745185f91
Enigmater <ivan.tihonov1999@mail.ru>
2025-02-20 00:05:28 +0700

    Правка адреса и UI * Изменил адрес сервера * До конца изменил подстраивание под темы в Авторизации

--- DatingAppClient/app/src/main/java/com/example/datingappclient/constants/Constants.java
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.example.datingappclient.constants;
 
 public class Constants {
-    public final static String SERVER_ADDRESS = "26.223.19.56";
+    public final static String SERVER_ADDRESS = "45.12.255.26";
     public final static String SERVER_PORT = "8080";
 }
