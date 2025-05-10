1505ab5a2b4598ca1ce277bde4d23d8ddc8858fa
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-05-10 17:44:46 +0700

    Solved Websockets problem and deleted JwtHandshakeInterceptor

--- src/main/java/com/datingapp/datingapp/config/WebSocketConfig.java
+++ src/main/java/com/datingapp/datingapp/config/WebSocketConfig.java
@@ -1,20 +1,12 @@
 package com.datingapp.datingapp.config;
 
 import com.datingapp.datingapp.security.AuthChannelInterceptor;
-import com.datingapp.datingapp.security.JwtHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
-import org.springframework.http.server.ServerHttpRequest;
-
-import java.security.Principal;
-import java.util.Map;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
