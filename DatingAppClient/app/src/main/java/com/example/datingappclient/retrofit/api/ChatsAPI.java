5a73459eddfaecd1313676c120dcee04205d2da5
Enigmater <ivan.tihonov1999@mail.ru>
2025-05-12 22:36:32 +0700

    Добавил запросы для работы с гр.чатами

--- DatingAppClient/app/src/main/java/com/example/datingappclient/retrofit/api/ChatsAPI.java
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/retrofit/api/ChatsAPI.java
@@ -1,23 +1,39 @@
 package com.example.datingappclient.retrofit.api;
 
 import com.example.datingappclient.model.dto.ChatDTO;
+import com.example.datingappclient.model.dto.GroupChatDTO;
+import com.example.datingappclient.model.dto.GroupChatInfoDTO;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ChatsAPI {
-    @GET("api/chats/{pk_user}")
-    Call<List<ChatDTO>> getChats(@Path("pk_user") int userId);
+    @GET("api/chats/{userId}")
+    Call<List<ChatDTO>> getChats(@Path("userId") int userId);
 
-    @GET("api/chats/{pk_chat}/users/{pk_user}")
-    Call<ChatDTO> getChat(@Path("pk_user") int userId, @Path("pk_chat") int chatId);
+    @GET("api/chats/{chatId}/users/{userId}")
+    Call<ChatDTO> getChat(@Path("userId") int userId, @Path("chatId") int chatId);
 
     // Создание чата с юзером
     @POST("api/chats")
     Call<Integer> createChat(@Query("userA") int userId, @Query("userB") int likerId);
+
+    /* === Group Chats === */
+    @GET("api/group_chats/{chatId}")
+    Call<GroupChatInfoDTO> getChatInfo(@Path("chatId") int chatId);
+
+    @GET("api/group_chats/users/{userId}")
+    Call< List<GroupChatDTO>> getGroupChats(@Path("userId") int userId);
+
+    @POST("/api/group_chats")
+    Call <Void> addChat(@Body GroupChatDTO groupChatDto);
+
+    @POST("/api/group_chats/{chatId}/users/{userId}")
+    Call <Void> addMemberToChat(@Path("chatId") int chatId, @Path("userId") int userId);
 }
