9db833012a58ef39c7922259c4134bb9d19ce99a
Enigmater <ivan.tihonov1999@mail.ru>
2025-05-12 20:13:09 +0700

    Изменил системный сплеш (проверка токена еще не добавлена)

--- DatingAppClient/app/src/main/java/com/example/datingappclient/activity/AuthActivity.java
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/activity/AuthActivity.java
@@ -3,22 +3,21 @@ package com.example.datingappclient.activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 
+import androidx.core.splashscreen.SplashScreen;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.datingappclient.R;
-import com.example.datingappclient.constants.Constants;
 import com.example.datingappclient.fragments.SigninFragment;
 import com.example.datingappclient.model.AuthResponse;
 
 public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-
         getSupportFragmentManager().beginTransaction().replace(R.id.auth_fragment_container, new SigninFragment()).commit();
 
         /*String logTag = Constants.GLOBAL_LOG_TAG + "AUTH CHECK";
