f46d6c42560114a0a1ebf210d76f56d120ac7b43
Enigmater <ivan.tihonov1999@mail.ru>
2025-05-13 23:49:14 +0700

    Добавил форму для создания гр.чата * указать наименование * выбор фотки (еще не реализовано)

---
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/fragments/CreateGroupChatFragment.java
@@ -0,0 +1,71 @@
+package com.example.datingappclient.fragments;
+
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.datingappclient.R;
+
+public class CreateGroupChatFragment extends Fragment {
+
+    private View activityView;
+
+    public CreateGroupChatFragment() {};
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activityView = inflater.inflate(R.layout.fragment_create_groupchat, container, false);
+
+        Toolbar toolbar = activityView.findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(v -> {
+            getParentFragmentManager().popBackStack();
+        });
+
+        animUnderlineEdit();
+
+        return activityView;
+    }
+
+    private void animUnderlineEdit() {
+        EditText editText = activityView.findViewById(R.id.chatNameEditText);
+        View underline = activityView.findViewById(R.id.underlineView);
+
+        editText.setOnFocusChangeListener((v, hasFocus) -> {
+            int colorFrom = ((ColorDrawable) underline.getBackground()).getColor();
+            int colorTo = hasFocus
+                    ? ContextCompat.getColor(requireContext(), R.color.green) // TODO: подогнать под темыы
+                    : ContextCompat.getColor(requireContext(), R.color.gray);
+
+            // Анимация цвета
+            ValueAnimator colorAnim = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+            colorAnim.setDuration(200);
+            colorAnim.addUpdateListener(anim -> underline.setBackgroundColor((int) anim.getAnimatedValue()));
+
+            // Анимация смещения по Y (вверх при фокусе, вниз при потере)
+            float fromY = underline.getTranslationY();
+            float toY = hasFocus ? -10f : 0f; // поднимаем на 6dp (примерно)
+
+            ObjectAnimator moveAnim = ObjectAnimator.ofFloat(underline, "translationY", fromY, toY);
+            moveAnim.setDuration(200);
+
+            // Одновременный запуск
+            AnimatorSet animatorSet = new AnimatorSet();
+            animatorSet.playTogether(colorAnim, moveAnim);
+            animatorSet.start();
+        });
+    }
+}
