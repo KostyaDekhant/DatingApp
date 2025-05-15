091e5d2948a5459effc16fac3395855263e1efb8
Kostya Dekhant <dekhant.kostya1@gmail.com>
2025-05-15 14:58:37 +0700

    Fixed req for received likes

--- src/main/java/com/datingapp/datingapp/repository/LikeRepo.java
+++ src/main/java/com/datingapp/datingapp/repository/LikeRepo.java
@@ -31,17 +31,13 @@ public interface LikeRepo extends JpaRepository<Like, Integer> {
     List<Object[]> findByLiker(@Param("id") int user_id);
 
     @Query(value = """
-SELECT l.liker, l.time, u.name, p.image, u.birthday 
-            FROM \"like\" l 
-            JOIN \"user\" u ON u.pk_user = l.liker 
-            LEFT JOIN \"user_pic\" up ON u.pk_user = up.pk_user 
-            LEFT JOIN \"picture\" p ON p.pk_picture = (  
-                SELECT MIN(pk_picture) 
-                FROM \"user_pic\" 
-                WHERE up.pk_user = u.pk_user  
-            )
-            WHERE l.poster = :id 
-            GROUP BY l.liker, l.time, u.name, p.image, u.birthday;
+SELECT l.liker, l.time, u.name, p.image, u.birthday
+FROM "like" l
+JOIN "user" u ON u.pk_user = l.liker
+LEFT JOIN "user_pic" up ON u.pk_user = up.pk_user
+LEFT JOIN "picture" p ON p.pk_picture = up.pk_picture AND p.id = 1
+WHERE l.poster = :id
+GROUP BY l.liker, l.time, u.name, p.image, u.birthday;
 """, nativeQuery = true)
     List<Object[]> findByReceiver(@Param("id") int user_id); //LikeDTO
 
