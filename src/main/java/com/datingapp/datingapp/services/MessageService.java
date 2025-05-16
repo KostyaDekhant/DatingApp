74853bd97bb516301806e1d691a191e8ae06e757
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-05-16 12:16:34 +0700

    Fixed getChatHistory

--- src/main/java/com/datingapp/datingapp/services/MessageService.java
+++ src/main/java/com/datingapp/datingapp/services/MessageService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,7 +26,7 @@ public class MessageService {
 
     public List<MessageDTO> getChatHistory(int chatId) {
         List<Message> messages = messRepo.findChatMessages(chatId);
-        List<MessageDTO> messageDTO = List.of();
+        List<MessageDTO> messageDTO = new ArrayList<>();
         for (Message message : messages) {
             messageDTO.add(new MessageDTO(message));
         }
