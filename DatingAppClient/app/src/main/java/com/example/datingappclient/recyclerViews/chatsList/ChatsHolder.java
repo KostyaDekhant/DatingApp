ddd58b23a9d16641c5b7d5c2939e26cc8ec86ebb
Enigmater <ivan.tihonov1999@mail.ru>
2025-05-27 16:14:47 +0700

    fix: update chat image

--- DatingAppClient/app/src/main/java/com/example/datingappclient/recyclerViews/chatsList/ChatsHolder.java
+++ DatingAppClient/app/src/main/java/com/example/datingappclient/recyclerViews/chatsList/ChatsHolder.java
@@ -3,6 +3,7 @@ package com.example.datingappclient.recyclerViews.chatsList;
 import static android.view.View.VISIBLE;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.datingappclient.R;
+import com.example.datingappclient.constants.Constants;
+import com.example.datingappclient.model.dto.ChatDTO;
 import com.example.datingappclient.model.dto.MessageDTO;
 import com.example.datingappclient.retrofit.repository.ChatsRepository;
 import com.example.datingappclient.retrofit.wrapper.Result;
@@ -46,15 +49,31 @@ public class ChatsHolder extends RecyclerView.ViewHolder {
     }
 
     public void subcribeToUpdateChat(int chatId, ChatsViewModel viewModel, ChatsRepository chatsRepository, int senderId) {
+        String logTag = Constants.GLOBAL_LOG_TAG + "UPDATE CHAT";
         AtomicReference<Disposable> disposableRef = new AtomicReference<>();
-        disposable = viewModel.subscribeToUpdateChat(chatId, result1 -> {
-            chatsRepository.fetchChat(chatId, senderId, result -> {
-                viewModel.updateOrAddChat(result.data);
 
-                Disposable d = disposableRef.get();
-                if (d != null && !d.isDisposed()) {
-                    d.dispose();
-                }
+        disposable = viewModel.subscribeToUpdateChat(chatId, result1 -> {
+            Log.d(logTag, "Ивент обновления чата внутри холдера " + chatId);
+
+            chatsRepository.fetchChat(chatId, senderId, chatResult -> {
+                if (chatResult.status != Result.Status.SUCCESS || chatResult.data == null) return;
+
+                ChatDTO chat = chatResult.data;
+
+                chatsRepository.fetchChatAvatar(senderId, chatId, avatarResult -> {
+                    if (avatarResult.status == Result.Status.SUCCESS && !avatarResult.data.isEmpty()) {
+                        chat.setImage(avatarResult.data.get(0).getImage());
+                    }
+
+                    // Теперь обновляем чат
+                    viewModel.updateOrAddChat(chat);
+
+                    // Отписываемся от обновления, если нужно
+                    Disposable d = disposableRef.get();
+                    if (d != null && !d.isDisposed()) {
+                        d.dispose();
+                    }
+                });
             });
         });
     }
