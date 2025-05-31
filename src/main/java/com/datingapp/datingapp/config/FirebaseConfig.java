412b5c0e1cfd09fb3a7c65ac5f1a987fdebf6369
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-06-01 01:22:51 +0700

    another fix for fcm filepath

--- src/main/java/com/datingapp/datingapp/config/FirebaseConfig.java
+++ src/main/java/com/datingapp/datingapp/config/FirebaseConfig.java
@@ -1,5 +1,6 @@
 package com.datingapp.datingapp.config;
 
+import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -10,6 +11,8 @@ import java.io.FileInputStream;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${firebase.service-account-file}")
+    private String firebaseKeyPath;
 
     @PostConstruct
     public void initialize() {
